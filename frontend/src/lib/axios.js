b3dea633b7431f54298e41743e0104e4ef7105a3
Vivek Chavan <vivekchavan382@gmail.com>
2024-12-31 14:46:25 +0530

    fix

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: import.meta.mode === "development" ? "http://localhost:5000/api" : "/api",
+	baseURL: import.meta.mode === "development" ? "http://82.112.231.201:5000/api" : "/api",
 	withCredentials: true, // send cookies to the server
 });
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
 	server: {
 		proxy: {
 			"/api": {
-				target: "http://localhost:5000",
+				target: "http://82.112.231.201:5000",
 			},
 		},
 	},
